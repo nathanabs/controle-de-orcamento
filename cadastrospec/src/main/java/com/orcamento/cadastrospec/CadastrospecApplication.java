4e51e042c762d3ef5bb5bb73df040c310971ecf0
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2023-01-28 15:33:33 -0300

    criando projeto

---
+++ cadastrospec/src/main/java/com/orcamento/cadastrospec/CadastrospecApplication.java
@@ -0,0 +1,13 @@
+package com.orcamento.cadastrospec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrospecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrospecApplication.class, args);
+	}
+
+}
