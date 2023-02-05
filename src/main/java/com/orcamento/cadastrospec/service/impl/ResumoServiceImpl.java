dc15bcec1f6b4ca3ce0f636c47e4e483e725064e
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2023-02-05 16:23:50 -0300

    Resumo de despesa mensal

--- src/main/java/com/orcamento/cadastrospec/service/impl/ResumoServiceImpl.java
+++ src/main/java/com/orcamento/cadastrospec/service/impl/ResumoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.orcamento.cadastrospec.service.impl;
 
+import com.orcamento.cadastrospec.enums.Categoria;
 import com.orcamento.cadastrospec.model.Despesa;
 import com.orcamento.cadastrospec.model.Receita;
+import com.orcamento.cadastrospec.model.ResumoCategoria;
 import com.orcamento.cadastrospec.model.ResumoResponse;
 import com.orcamento.cadastrospec.repositories.DespesaRepository;
 import com.orcamento.cadastrospec.repositories.ReceitaRepository;
@@ -11,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,15 +32,34 @@ public class ResumoServiceImpl implements ResumoService {
         var despesas = despesaRepository.buscarReceitasMensais(dataInicial, dataFinal);
 
         var totalReceitas = receitas.stream().map(Receita::getValor).reduce(BigDecimal.ZERO, BigDecimal::add);
-        var totalDespesas = despesas.stream().map(Despesa::getValor).reduce(BigDecimal.ZERO, BigDecimal::add);
+        var totalDespesas = getTotalDespesas(despesas);
 
         var saldo = totalReceitas.subtract(totalDespesas);
-
 
         return ResumoResponse.builder()
                 .totalReceita(totalReceitas)
                 .totalDespesa(totalDespesas)
                 .saldoFinal(saldo)
+                .valorCategoria(resumoDespesaPorCategoria(despesas))
+                .build();
+    }
+
+    private static BigDecimal getTotalDespesas(List<Despesa> despesas) {
+        return despesas.stream().map(Despesa::getValor).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private List<ResumoCategoria> resumoDespesaPorCategoria(List<Despesa> despesas){
+        var despesaCategoria = despesas.stream().collect(Collectors.groupingBy(Despesa::getCategoria));
+
+        return despesaCategoria.entrySet().stream()
+                .map(entry -> getResumoDespesa(entry.getKey(), entry.getValue()))
+                .toList();
+    }
+
+    private ResumoCategoria getResumoDespesa(Categoria categoria, List<Despesa> despesas){
+        return ResumoCategoria.builder()
+                .categoria(com.orcamento.cadastrospec.model.Categoria.fromValue(categoria.getValue()))
+                .valor(getTotalDespesas(despesas))
                 .build();
     }
 }
