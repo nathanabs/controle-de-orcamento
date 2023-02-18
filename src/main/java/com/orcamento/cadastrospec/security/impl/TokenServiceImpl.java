a7f6a34481cc174c9f4b454954b22f0ecf5e1c3c
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2023-02-18 18:32:08 -0300

    Criando token jwt

--- src/main/java/com/orcamento/cadastrospec/security/impl/TokenServiceImpl.java
+++ src/main/java/com/orcamento/cadastrospec/security/impl/TokenServiceImpl.java
@@ -6,19 +6,18 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.orcamento.cadastrospec.model.Usuario;
 import com.orcamento.cadastrospec.security.TokenService;
 import com.orcamento.cadastrospec.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
 
 @Service
 public class TokenServiceImpl implements TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
     @Override
     public String gerarToken(Usuario usuario) {
         try {
-            var algoritimo = Algorithm.HMAC256("1234456");
+            var algoritimo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API controle de orcamento")
                     .withSubject(usuario.getLogin())
